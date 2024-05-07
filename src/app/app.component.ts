469a50d5413aae36f9f05642cd33629097b2781d
Rafael Benavides Terrón <rbenavidest@nightgates.com>
2024-05-07 13:33:38 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { GridOptions, GridReadyEvent } from 'ag-grid-community';
+import 'ag-grid-enterprise';
+import { ColumnFilterComponent } from './components/column-filter/column-filter.component';
+import { AgGridAngular } from 'ag-grid-angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+  public gridOptions: GridOptions = {
+    defaultColDef: {
+      floatingFilter: true
+    },
+    columnDefs: [
+      { field: "athlete", rowGroup: true, filter: ColumnFilterComponent },
+      { field: "year" },
+      { field: "country" },
+      { field: "sport" },
+      { field: "gold" },
+      { field: "silver" },
+      { field: "bronze" },
+      { field: "total" },
+    ],
+    autoGroupColumnDef: {
+      filter: "agGroupColumnFilter"
+    },
+    onGridReady: (params: GridReadyEvent) => this.onGridReady(params)
+  };
+
+  @ViewChild(AgGridAngular)
+  public grid!: AgGridAngular;
+
+  constructor(private http: HttpClient) { }
+
+  onGridReady(params: GridReadyEvent<any>) {
+    this.http
+      .get<any>("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .subscribe((data) => {
+        this.grid.api.setGridOption('rowData', data);
+      });
+  }
+}
---
+++ src/app/components/column-filter/column-filter.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { IFilterAngularComp } from 'ag-grid-angular';
+import { IFilterParams, AgPromise, IDoesFilterPassParams, IAfterGuiAttachedParams } from 'ag-grid-community';
+
+@Component({
+  selector: 'app-column-filter',
+  templateUrl: './column-filter.component.html',
+  styleUrl: './column-filter.component.scss'
+})
+export class ColumnFilterComponent implements IFilterAngularComp {
+
+  constructor() { }
+
+  agInit(params: IFilterParams<any, any>): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  isFilterActive(): boolean {
+    return false;
+    // throw new Error('Method not implemented.');
+  }
+
+  getModel() {
+    // throw new Error('Method not implemented.');
+  }
+
+  setModel(model: any): void | AgPromise<void> {
+    // throw new Error('Method not implemented.');
+  }
+
+  // refresh?(newParams: IFilterParams<any, any>): boolean {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  doesFilterPass(params: IDoesFilterPassParams<any>): boolean {
+    return true;
+    // throw new Error('Method not implemented.');
+  }
+
+  // onNewRowsLoaded?(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // onAnyFilterChanged?(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // getModelAsString?(model: any): string {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // afterGuiAttached?(params?: IAfterGuiAttachedParams | undefined): void {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // afterGuiDetached?(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+
+}
